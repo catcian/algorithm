d76bd8136fc321c81d8a5c2a25b55dc46bb5060c
wangchn86@163.com <wangchn86@163.com>
2020-07-31 07:15:53 +0800

    线性查找-范型

--- LinearSearch/src2-3/LinearSearch.java
+++ LinearSearch/src2-3/LinearSearch.java
@@ -4,19 +4,19 @@ public class LinearSearch {
     private LinearSearch() {
     }
 
-    public static <E> int search(E[] data, E target) {
+    public static int search(int[] data, int target) {
 
         for (int i = 0; i < data.length; i++)
-            if (data[i].equals(target))
+            if (data[i] == target)
                 return i;
         return -1;
     }
 
     public static void main(String[] args) {
 
-        Integer[] data = {24, 18, 12, 9, 16, 66, 32, 4};
+        int[] data = {24, 18, 12, 9, 16, 66, 32, 4};
         LinearSearch ls = new LinearSearch();
-        int res = LinearSearch.<Integer>search(data, 16);
+        int res = LinearSearch.search(data, 16);
         System.out.println(res);
         int res2 = LinearSearch.search(data, 666);
         System.out.println(res2);
